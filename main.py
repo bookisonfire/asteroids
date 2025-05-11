12d733a38bc82001e048ea05f6439e131c3b5006
bookisonfire <dbook7@proton.me>
2025-05-10 22:32:54 -0400

    B: IT'S ALIVE!

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import pygame 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0 # Placeholder for delta time
+    clock = pygame.time.Clock()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+        
     while True:
+        updateable.update(dt)
         screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
+        dt = clock.tick(60) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__": 
     main()
